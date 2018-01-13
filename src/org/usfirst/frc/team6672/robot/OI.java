68eb8e99b4c07960117effefd9ffbfccf9ef8058
Winston Hartnett <WinstonHartnett@users.noreply.github.com>
2018-01-12 18:08:11 -0600

    Old robot code fixed up and uploaded
    Changes the DriveBase subsystem code to use the new DifferentialDrive class instead of the (now) deprecated RobotDrive class.

--- src/org/usfirst/frc/team6672/robot/OI.java
+++ src/org/usfirst/frc/team6672/robot/OI.java
@@ -1,11 +1,12 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package org.usfirst.frc.team6672.robot;
+
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import edu.wpi.first.wpilibj.buttons.Trigger;
+import edu.wpi.first.wpilibj.Joystick;
+
+import org.usfirst.frc.team6672.robot.commands.*;
+import org.usfirst.frc.team6672.robot.triggers.*;
 
 /**
  * This class is the glue that binds the controls on the physical operator
@@ -13,9 +14,10 @@ package org.usfirst.frc.team6672.robot;
  */
 
 public class OI {
+	
 	//// CREATING BUTTONS
 	// One type of button is a joystick button which is any button on a
-	//// joystick.
+	// joystick.
 	// You create one by telling it which joystick it's on and which button
 	// number it is.
 	// Joystick stick = new Joystick(port);
@@ -40,4 +42,59 @@ public class OI {
 	// Start the command when the button is released and let it run the command
 	// until it is finished as determined by it's isFinished method.
 	// button.whenReleased(new ExampleCommand());
+	
+	Joystick stick = new Joystick(1);
+	Button button1 = new JoystickButton(stick, 1);
+	Button button2 = new JoystickButton(stick, 2);
+	Button button3 = new JoystickButton(stick, 3);
+	Button button4 = new JoystickButton(stick, 4);
+	Button button5 = new JoystickButton(stick, 5);
+	Button button6 = new JoystickButton(stick, 6);
+	Button button7 = new JoystickButton(stick, 7);
+	
+	Trigger autoGearOpen = new AutoGearOpen();
+	Trigger autoGearClose = new AutoGearClose();
+	Trigger manualGearOpen = new ManualGearOpen();
+	Trigger manualGearClose = new ManualGearClose();
+	
+	public double getStickAxis1(){
+		double stickAxis1 = stick.getRawAxis(1);
+		return stickAxis1;
+	}
+	
+	public double getStickAxis4(){
+		double stickAxis4 = stick.getRawAxis(4);
+		return stickAxis4;
+	}
+	
+	public boolean autoGearOpenPressed() {
+		return stick.getRawAxis(3) > .5 ;
+	}
+	
+	public boolean autoGearClosePressed() {
+		return stick.getRawAxis(2) > .5 ;
+	}
+	
+	public boolean manualGearOpenActivated() {
+		return button7.get() && stick.getRawAxis(3) > .5 ;
+	}
+	
+	public boolean manualGearCloseActivated() {
+		return button7.get() && stick.getRawAxis(2) > .5 ;
+	}
+	
+	public OI(){
+		autoGearOpen.whenActive(new OpenGearHolderOnce());
+		autoGearClose.whenActive(new CloseGearHolderOnce());
+		manualGearOpen.whileActive(new OpenGearHolderContinued());
+		manualGearClose.whileActive(new CloseGearHolderContinued());
+		button3.whenPressed(new RotateSpeed6());
+		button2.whenPressed(new RotateSpeed7());
+		button1.whenPressed(new RotateSpeed10());
+		button4.whileHeld(new TurnRopeClimber());
+		button5.whenPressed(new DriveSpeed8());
+		button6.whenPressed(new DriveSpeed10());
+		
+	}
+	
 }
